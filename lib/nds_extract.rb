c788de94dbee9be6b6a832b2be0baa73f3cde547
nickie kane <kameo712@gmail.com>
2020-01-03 03:25:23 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,9 +1,22 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
+require 'pry'
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
+    # pp director_data
+    row_index = 0
+    d_total = 0
+    while row_index < director_data[:movies].length do
+      total = director_data[:movies][row_index][:worldwide_gross]
+      d_total += total
+      
+    
+    row_index += 1
+    end
+    return d_total
+    
 
 end
 
@@ -12,6 +25,23 @@ end
 #
 # { directorOne => allTheMoneyTheyMade, ... }
 def directors_totals(nds)
+  # pp nds
+  nds
   result = {}
-  nil
+  director = nds[0][:name]
+  result[director] = {}
+  row_index = 0
+  grand_total = 0 
+  while row_index < directors_database[0][:movies].length do
+  total = directors_database[0][:movies][row_index][:worldwide_gross]
+  grand_total += total
+  
+  row_index += 1
+  end
+  binding.pry
+  return grand_total
+  
+  
+  # binding.pry
+  # nil
 end
